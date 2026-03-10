243cb2ccd0f5b2044d9241845a22441133e2fe23
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-03-10 10:27:15 -0400

    working 9-1

--- restaurant.py
+++ restaurant.py
@@ -3,17 +3,17 @@ class Restaurant:
     
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
-        self.cuising_type = self.cuising_type
+        self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        print(f"The restaurant is named {self.restaurant_name} and it sells {self.cuising_type} food.")
+        print(f"The restaurant is named {self.restaurant_name} and it sells {self.cuisine_type} food.")
 
     def open_restaurant(self):
-        print(f"{self.restuarant_name} is open for business!")
+        print(f"{self.restaurant_name} is open for business!")
 
-    chick_fil_a = Restaurant("Chick fil a", "Fast")
+chick_fil_a = Restaurant("Chick fil a", "Fast")
 
-    print(chick_fil_a.cuisine_type)
-    print(chick_fil_a.restaurant_name)
-    
-    
+print(chick_fil_a.cuisine_type)
+print(chick_fil_a.restaurant_name)
+print(chick_fil_a.describe_restaurant())
+print(chick_fil_a.open_restaurant())
